51a9ba77f194505de392c74f5a209dbd484e5a35
Yuri Nazarenko <yuri@nazarenko.cc>
2015-10-05 16:11:38 +0300

    - Корректная инсталяция nginx

--- .provision/nginx.sh
+++ .provision/nginx.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 apt-get install nginx -y
+sed -i 's/user www\-user;/user vagrant;/g' /etc/nginx/nginx.conf
+if [ ! -f /vagrant/nginx.conf ]
+	then
+		cp nginx.conf /vagrant
+fi
+if [ `grep "^include /vagrant/nginx.conf" /etc/nginx/nginx.conf | wc -l` = 0 ]
+	then
+		echo "include /vagrant/nginx.conf" >> /etc/nginx/nginx.conf
+fi
