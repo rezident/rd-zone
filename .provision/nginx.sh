2b8b90022f4ad9311d78bcfb97665c90d9a3652c
Yuri Nazarenko <yuri@nazarenko.cc>
2015-10-05 16:54:17 +0300

    - Исправление добавления include nginx.conf в секцию http (v3)

--- .provision/nginx.sh
+++ .provision/nginx.sh
@@ -5,7 +5,7 @@ if [ ! -f /vagrant/nginx.conf ]
 	then
 		cp nginx.conf /vagrant
 fi
-rm -f /etc/nginx/sites/enabled/default
+rm -f /etc/nginx/sites-enabled/default
 echo "include /vagrant/nginx.conf" > /etc/nginx/sites-enabled/vagrant.conf
 
 /etc/init.d/nginx restart
