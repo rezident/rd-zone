ea176777e270514570e5d7ca60964a155668ed0f
Yuri Nazarenko <yuri@nazarenko.cc>
2015-10-05 16:20:32 +0300

    - Добавление include nginx.conf в секцию http

--- .provision/nginx.sh
+++ .provision/nginx.sh
@@ -5,8 +5,8 @@ if [ ! -f /vagrant/nginx.conf ]
 	then
 		cp nginx.conf /vagrant
 fi
-if [ `grep "^include /vagrant/nginx.conf" /etc/nginx/nginx.conf | wc -l` = 0 ]
+if [ `grep "include /vagrant/nginx.conf" /etc/nginx/nginx.conf | wc -l` = 0 ]
 	then
-		echo "include /vagrant/nginx.conf" >> /etc/nginx/nginx.conf
+		printf "http {\n\tinclude /vagrant/nginx.conf\n}" >> /etc/nginx/nginx.conf
 fi
 /etc/init.d/nginx restart
