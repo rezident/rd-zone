eac45d756d1c72336da371b6d42ac232bff83821
Yuri Nazarenko <yuri@nazarenko.cc>
2015-10-08 11:49:18 +0300

    - Добавление в автозапуск перезагрузку php5-fpm для подхватывания дополнительных настроек из php.ini - Обновлена инструкция

--- .provision/php5-fpm.sh
+++ .provision/php5-fpm.sh
@@ -6,12 +6,16 @@ sed -i 's/^group = www-data/group = vagrant/g' /etc/php5/fpm/pool.d/www.conf
 sed -i 's/^listen.owner = www-data/listen.owner = vagrant/g' /etc/php5/fpm/pool.d/www.conf
 sed -i 's/^listen.group = www-data/listen.group = vagrant/g' /etc/php5/fpm/pool.d/www.conf
 
-
 if [ ! -f /vagrant/php.ini ]
 	then
 		touch /vagrant/php.ini
 fi
 
 ln -sf /vagrant/php.ini /etc/php5/fpm/conf.d/vagrant.ini
+
+if [ `grep "/etc/init.d/php5-fpm restart" /root/autorun.sh | wc -l` = 0 ]
+	then
+	echo "/etc/init.d/php5-fpm restart" > /root/autorun.sh
+fi
 
 /etc/init.d/php5-fpm restart
