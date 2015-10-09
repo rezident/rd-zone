40403db97780f25e1e712411d5d452c0909a8bf3
Yuri Nazarenko <mail@rezident.org>
2015-10-09 23:49:31 +0300

    - Добавление установки mysql-server с хранением баз данных в папке vagrant

--- .provision/mysql-server.sh
+++ .provision/mysql-server.sh
@@ -4,3 +4,21 @@ echo "mysql-server mysql-server/root_password password vagrant" | debconf-set-se
 echo "mysql-server mysql-server/root_password_again password vagrant" | debconf-set-selections
 
 apt-get install mysql-server -y
+
+/etc/init.d/mysql stop
+mkdir -p /vagrant/.provision
+mv /var/lib/mysql /vagrant/.provision
+
+sed -i 's/= mysql$/= root/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i 's/= \/var\/lib\/mysql$/= /vagrant\/.provision\/mysql/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i 's/^User=mysql$/User=root/g' /lib/systemd/system/mysql.service
+sed -i 's/^Group=mysql$/Group=root/g' /lib/systemd/system/mysql.service
+
+systemctl daemon-reload
+
+if [ `grep "/etc/init.d/mysql restart" /root/autorun.sh | wc -l` = 0 ]
+	then
+	echo "/etc/init.d/mysql restart" >> /root/autorun.sh
+fi
+
+/etc/init.d/mysql start
