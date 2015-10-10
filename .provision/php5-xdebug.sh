2317683c7d0e071973c768dfbe056d346121d427
Yuri Nazarenko <mail@rezident.org>
2015-10-10 18:01:46 +0300

    - Создание символических ссылок на конфиг xdebug с ключом force

--- .provision/php5-xdebug.sh
+++ .provision/php5-xdebug.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get install php5-xdebug -y
-ln -s /etc/php5/conf-available/xdebug.ini /etc/php5/cli/conf.d/20-xdebug.ini
-ln -s /etc/php5/conf-available/xdebug.ini /etc/php5/fpm/conf.d/20-xdebug.ini
+ln -sf /etc/php5/conf-available/xdebug.ini /etc/php5/cli/conf.d/20-xdebug.ini
+ln -sf /etc/php5/conf-available/xdebug.ini /etc/php5/fpm/conf.d/20-xdebug.ini
 
 if [ -f /etc/init.d/php5-fpm ]
     then /etc/init.d/php5-fpm restart
