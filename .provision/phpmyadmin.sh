58ba76381b2568502074cf126382e9afdd2d7d5e
Yuri Nazarenko <mail@rezident.org>
2015-10-11 20:47:09 +0300

    - Прописывание автовхода для phpmyadmin

--- .provision/phpmyadmin.sh
+++ .provision/phpmyadmin.sh
@@ -22,4 +22,12 @@ apt-get install phpmyadmin -y
 
 ln -sf `pwd`/phpmyadmin.nginx.conf /etc/nginx/sites-enabled/phpmyadmin.conf
 
+AUTO_ENTER="\$cfg['Servers'][\$i]['auth_type'] = 'config';";
+if [ `grep "^$AUTO_ENTER" /etc/phpmyadmin/config.inc.php | wc -l` = 0 ]
+    then
+    echo $AUTO_ENTER >> /etc/phpmyadmin/config.inc.php
+    echo "\$cfg['Servers'][\$i]['user'] = 'root';" >> /etc/phpmyadmin/config.inc.php
+    echo "\$cfg['Servers'][\$i]['password'] = 'vagrant';" >> /etc/phpmyadmin/config.inc.php
+fi
+
 /etc/init.d/nginx restart
